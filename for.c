f8d9a6ae02c8f58eea6195d3bac7b773dd3a3974
huihui-wang <huihui.wang@soarwhale.com>
2025-07-17 16:05:05 +0800

    add a new file named for.c

---
+++ for.c
@@ -0,0 +1,28 @@
+/*
+#include <stdio.h>
+
+int main(void) {
+    for (int i = 0; i < 5; i++) {
+        printf("i = %d\n", i);
+    }
+    return 0;
+}
+
+*/
+
+
+#include <stdio.h>
+
+static int count = 0;
+
+void counter(void) {
+    count++;
+    printf("%d\n", count);
+}
+
+int main(void) {
+    counter();   /* 输出 1 */
+    counter();   /* 输出 2 */
+    counter();   /* 输出 3 */
+    return 0;
+}
