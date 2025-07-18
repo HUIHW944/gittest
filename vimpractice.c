15af326522a4c511a6b9fee3efcd44f3e37ec339
huihui-wang <huihui.wang@soarwhale.com>
2025-07-18 14:29:56 +0800

    add a new file named vimpractice.c

---
+++ vimpractice.c
@@ -0,0 +1,10 @@
+const typing () => {
+  // this is a description
+  //
+  //   another description 
+  const timer   = setTimeout(() => {
+    console.log('that') 
+    alert('cool!')
+    // awesome man !
+  } 300);
+};
