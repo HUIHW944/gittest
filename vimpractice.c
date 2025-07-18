7f9122ea3c5ee4803e26df128e5ed071e8204560
huihui-wang <huihui.wang@soarwhale.com>
2025-07-18 15:28:03 +0800

    update file

--- vimpractice.c
+++ vimpractice.c
@@ -1,10 +1,15 @@
-const typing () => {
-  // this is a description
-  //
-  //   another description 
-  const timer   = setTimeout(() => {
-    console.log('that') 
-    alert('cool!')
-    // awesome man !
+/*
+   const typing () => {
+   const timer   = setTimeout(() => {
+   console.log('that') 
+   alert('cool!')
   } 300);
 };
+  */
+
+function(arg1,    ch)  // 在第二个参数（即 “    ch”）中输入 daa
+function(arg1|) // 就会变成这样，| 代表这时光标所在位置；如果这时再输入 daa
+function(|) // 就会变成这样
+
+function(arg1,    ch) // 在第二个参数（即 “    ch”）中输入 cia
+function(arg1,    |) // 就会变成这样，| 代表这时光标所在位置
